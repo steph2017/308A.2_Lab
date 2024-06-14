bfd3db965eb9748627483854e429a5bcecf5d091
StephOg <stephanieogwo@Stephanies-MBP.lan>
2024-06-14 14:09:14 -0400

    added rolle method to char class

--- index.js
+++ index.js
@@ -35,4 +35,8 @@ class Character {
         this.health = 100;
         this.inventory = [];
     }
+    roll(mod = 0) {
+        const result = Math.floor(Math.random() * 20) + 1 + mod;
+        console.log(`${this.name} rolled a ${result}.`)
+    }
 }
